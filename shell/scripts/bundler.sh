d25007db127d0c5f19fe08498780096a9ce437f1
Jussi Pekonen <jussi@pekonen.cc>
2022-08-16 09:49:03 +0300

    shell: Rename precheck to preface in the bundleScript function

--- shell/scripts/bundler.sh
+++ shell/scripts/bundler.sh
@@ -3,14 +3,14 @@
 # Script bundle function
 bundleScript() {
   output="$1"
-  precheck="$2"
+  preface="$2"
   shift 2
   input_files="$*"
   # Create script bundle
   echo "Bundling the script ${output}..."
   test -f "${output}" && rm -f "${output}"
-  # Add the pre-check, if requested by the parameters
-  test "${precheck}" != "" && echo "${precheck}" > "${output}"
+  # Add the preface if requested by the parameters
+  test "${preface}" != "" && echo "${preface}" > "${output}"
   for file in ${input_files}
   do
     test -f "${output}" && grep -E -v -e "^#(!/|shellcheck source)" -e "^(\.|source) " "${file}" >> "${output}"
@@ -19,5 +19,5 @@ bundleScript() {
   # Set the script executable
   echo "Setting the script ${output} executable..."
   chmod 0555 "${output}"
-  unset output precheck input_files file
+  unset output preface input_files file
 }
