873d55a89821127e6bbc0c083baf633e4bc20363
Jussi Pekonen <jussi@pekonen.cc>
2022-08-19 21:59:11 +0300

    shell: Rename some function variables in bundleScript to avoid potential confusion

--- shell/functions/bundler.sh
+++ shell/functions/bundler.sh
@@ -2,22 +2,22 @@
 
 # Script bundle function
 bundleScript() {
-  output="$1"
-  preface="$2"
+  __bundle_script_output="$1"
+  __bundle_script_preface="$2"
   shift 2
-  input_files="$*"
+  __bundle_script_input_files="$*"
   # Create script bundle
-  echo "Bundling the script ${output}..."
-  test -f "${output}" && rm -f "${output}"
+  echo "Bundling the script ${__bundle_script_output}..."
+  test -f "${__bundle_script_output}" && rm -f "${__bundle_script_output}"
   # Add the preface if requested by the parameters
-  test "${preface}" != "" && echo "${preface}" > "${output}"
-  for file in ${input_files}
+  test "${__bundle_script_preface}" != "" && echo "${__bundle_script_preface}" > "${__bundle_script_output}"
+  for __bundle_script_file in ${__bundle_script_input_files}
   do
-    test -f "${output}" && grep -E -v -e "^#(!/|shellcheck source)" -e "^(\.|source) " "${file}" >> "${output}"
-    test ! -f "${output}" && cat "${file}" >> "${output}"
+    test -f "${__bundle_script_output}" && grep -E -v -e "^#(!/|shellcheck source)" -e "^(\.|source) " "${__bundle_script_file}" >> "${__bundle_script_output}"
+    test ! -f "${__bundle_script_output}" && cat "${__bundle_script_file}" >> "${__bundle_script_output}"
   done
   # Set the script executable
-  echo "Setting the script ${output} executable..."
-  chmod 0555 "${output}"
-  unset output preface input_files file
+  echo "Setting the script ${__bundle_script_output} executable..."
+  chmod +x "${__bundle_script_output}"
+  unset __bundle_script_output __bundle_script_preface __bundle_script_input_files __bundle_script_file
 }
